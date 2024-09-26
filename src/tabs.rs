e319bdb5ca23a55350218dfc40b39cca99f0ab6b
aapelix <mcaapelix@gmail.com>
2024-09-26 17:20:30 +0300

    Trying some optimizations

--- src/tabs.rs
+++ src/tabs.rs
@@ -7,6 +7,106 @@ use webkit2gtk::{
     HitTestResultExt, WebViewExt,
 };
 
+const HOME_PAGE_HTML: &str = r##"
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>New tab</title>
+        <style>
+            body, html {
+                height: 100%;
+                margin: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: #202020;
+            }
+            .container {
+                text-align: center;
+            }
+            svg {
+                width: 100px;
+                height: 100px;
+                margin-bottom: 20px;
+            }
+            .darkened {
+                color: #888;
+            }
+            .hidden {
+                display: none;
+            }
+            h1 {
+                color: #f1f1f1;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div id="svg1" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-radar"><path d="M19.07 4.93A10 10 0 0 0 6.99 3.34"/><path d="M4 6h.01"/><path d="M2.29 9.62A10 10 0 1 0 21.31 8.35"/><path d="M16.24 7.76A6 6 0 1 0 8.23 16.67"/><path d="M12 18h.01"/><path d="M17.99 11.66A6 6 0 0 1 15.77 16.67"/><circle cx="12" cy="12" r="2"/><path d="m13.41 10.59 5.66-5.66"/></svg>
+            </div>
+            <div id="svg2" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-coffee"><path d="M10 2v2"/><path d="M14 2v2"/><path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"/><path d="M6 2v2"/></svg>
+            </div>
+            <div id="svg3" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>
+            </div>
+
+            <div id="svg4" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bomb"><circle cx="11" cy="13" r="9"/><path d="M14.35 4.65 16.3 2.7a2.41 2.41 0 0 1 3.4 0l1.6 1.6a2.4 2.4 0 0 1 0 3.4l-1.95 1.95"/><path d="m22 2-1.5 1.5"/></svg>
+            </div>
+
+            <div id="svg5" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bowl-chopsticks"><path d="m13 2-3 11"/><path d="m22 2-8 11"/><ellipse cx="12" cy="12" rx="10" ry="5"/><path d="M22 12a10 10 0 0 1-20 0"/></svg>
+            </div>
+
+            <div id="svg6" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bot"><path d="M12 8V4H8"/><rect width="16" height="12" x="4" y="8" rx="2"/><path d="M2 14h2"/><path d="M20 14h2"/><path d="M15 13v2"/><path d="M9 13v2"/></svg>
+            </div>
+
+            <div id="svg7" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-terminal"><path d="m7 11 2-2-2-2"/><path d="M11 13h4"/><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/></svg>
+            </div>
+
+            <div id="svg8" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-antenna"><path d="M2 12 7 2"/><path d="m7 12 5-10"/><path d="m12 12 5-10"/><path d="m17 12 5-10"/><path d="M4.5 7h15"/><path d="M12 16v6"/></svg>
+            </div>
+
+            <div id="random-svg-container"></div>
+
+            <h1>aapelix/abrw</h1>
+            <p class="darkened">F1 to open new tab</p>
+            <p class="darkened">F2 to open new window</p>
+        </div>
+
+        <script>
+            const svgIds = ['svg1', 'svg2', 'svg3', 'svg4', 'svg5', 'svg6', 'svg7', 'svg8'];
+
+            function getRandomSVG() {
+                const randomIndex = Math.floor(Math.random() * svgIds.length);
+                return svgIds[randomIndex];
+            }
+
+            function showRandomSVG() {
+                const randomSVGId = getRandomSVG();
+                const randomSVG = document.getElementById(randomSVGId);
+                const container = document.getElementById('random-svg-container');
+
+                const clonedSVG = randomSVG.cloneNode(true);
+                clonedSVG.classList.remove('hidden');
+
+                container.appendChild(clonedSVG);
+            }
+
+            showRandomSVG();
+        </script>
+    </body>
+    </html>
+
+    "##;
+
 pub fn add_tab(notebook: &Notebook, search_entry: &Entry, uri: Option<&str>) {
     let tab_box = Box::new(gtk::Orientation::Horizontal, 5);
     let tab_label = Label::new(Some("New tab"));
@@ -37,105 +137,7 @@ pub fn add_tab(notebook: &Notebook, search_entry: &Entry, uri: Option<&str>) {
             webview.load_uri(uri);
         }
         None => {
-            webview.load_html(r##"
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Home Page</title>
-                    <style>
-                        body, html {
-                            height: 100%;
-                            margin: 0;
-                            display: flex;
-                            justify-content: center;
-                            align-items: center;
-                            background-color: #202020;
-                        }
-                        .container {
-                            text-align: center;
-                        }
-                        svg {
-                            width: 100px;
-                            height: 100px;
-                            margin-bottom: 20px;
-                        }
-                        .darkened {
-                            color: #888;
-                        }
-                        .hidden {
-                            display: none;
-                        }
-                        h1 {
-                            color: #f1f1f1;
-                        }
-                    </style>
-                </head>
-                <body>
-                    <div class="container">
-                        <div id="svg1" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-radar"><path d="M19.07 4.93A10 10 0 0 0 6.99 3.34"/><path d="M4 6h.01"/><path d="M2.29 9.62A10 10 0 1 0 21.31 8.35"/><path d="M16.24 7.76A6 6 0 1 0 8.23 16.67"/><path d="M12 18h.01"/><path d="M17.99 11.66A6 6 0 0 1 15.77 16.67"/><circle cx="12" cy="12" r="2"/><path d="m13.41 10.59 5.66-5.66"/></svg>
-                        </div>
-                        <div id="svg2" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-coffee"><path d="M10 2v2"/><path d="M14 2v2"/><path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"/><path d="M6 2v2"/></svg>
-                        </div>
-                        <div id="svg3" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>
-                        </div>
-
-                        <div id="svg4" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bomb"><circle cx="11" cy="13" r="9"/><path d="M14.35 4.65 16.3 2.7a2.41 2.41 0 0 1 3.4 0l1.6 1.6a2.4 2.4 0 0 1 0 3.4l-1.95 1.95"/><path d="m22 2-1.5 1.5"/></svg>
-                        </div>
-
-                        <div id="svg5" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bowl-chopsticks"><path d="m13 2-3 11"/><path d="m22 2-8 11"/><ellipse cx="12" cy="12" rx="10" ry="5"/><path d="M22 12a10 10 0 0 1-20 0"/></svg>
-                        </div>
-
-                        <div id="svg6" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bot"><path d="M12 8V4H8"/><rect width="16" height="12" x="4" y="8" rx="2"/><path d="M2 14h2"/><path d="M20 14h2"/><path d="M15 13v2"/><path d="M9 13v2"/></svg>
-                        </div>
-
-                        <div id="svg7" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-terminal"><path d="m7 11 2-2-2-2"/><path d="M11 13h4"/><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/></svg>
-                        </div>
-
-                        <div id="svg8" class="hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-antenna"><path d="M2 12 7 2"/><path d="m7 12 5-10"/><path d="m12 12 5-10"/><path d="m17 12 5-10"/><path d="M4.5 7h15"/><path d="M12 16v6"/></svg>
-                        </div>
-
-                        <div id="random-svg-container"></div>
-
-                        <h1>aapelix/abrw</h1>
-                        <p class="darkened">F1 to open new tab</p>
-                        <p class="darkened">F2 to open new window</p>
-                    </div>
-
-                    <script>
-                        const svgIds = ['svg1', 'svg2', 'svg3', 'svg4', 'svg5', 'svg6', 'svg7', 'svg8'];
-
-                        function getRandomSVG() {
-                            const randomIndex = Math.floor(Math.random() * svgIds.length);
-                            return svgIds[randomIndex];
-                        }
-
-                        function showRandomSVG() {
-                            const randomSVGId = getRandomSVG();
-                            const randomSVG = document.getElementById(randomSVGId);
-                            const container = document.getElementById('random-svg-container');
-
-                            const clonedSVG = randomSVG.cloneNode(true);
-                            clonedSVG.classList.remove('hidden');
-
-                            container.appendChild(clonedSVG);
-                        }
-
-                        showRandomSVG();
-                    </script>
-                </body>
-                </html>
-
-                "##, None);
+            webview.load_html(HOME_PAGE_HTML, None);
         }
     }
 
@@ -146,12 +148,11 @@ pub fn add_tab(notebook: &Notebook, search_entry: &Entry, uri: Option<&str>) {
         }
     });
 
-    let max_length = 15;
-
     let notebook_clone = notebook.clone();
     webview.connect_title_notify(move |webview| {
         let notebook = notebook_clone.clone();
         let webview = webview.clone();
+        let max_length = 15;
 
         let title = webview
             .title()
